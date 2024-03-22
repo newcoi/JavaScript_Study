5d3c8ad1d07313cce0cf2156037aa50d5746fa65
Shirke <mrudula.shirke@siemens.com>
2024-03-22 11:14:33 +0530

    changes in operationarray

--- operationArray.js
+++ operationArray.js
@@ -1,5 +1,5 @@
 // combining two array, we can concatinate array. we can add multiple array
-const boys = ['Ram', 'sham', 'bharat'];
+const boys = ['Ram', 'sham', 'bharat', 'tana', 'bharat', 'raam', 'bharat'];
 const girls = ['Raama', 'sita', 'bharati'];
 
 const newArray = boys.concat(girls);
@@ -7,3 +7,18 @@ const newArray = boys.concat(girls);
 const newArray1 = boys.concat("bunny");
 
 console.log(newArray1);
+
+
+//finding index of value
+let elementIndex = boys.indexOf('bharat');
+console.log("index of bharat element is:", elementIndex);
+let lastElementIndex = boys.lastIndexOf('bharat');
+console.log("index of bharat element is:", lastElementIndex);
+
+//verify if element present
+let result = boys.includes("sham");
+console.log(result);
+
+//reversing array
+boys.reverse();
+console.log("After revesing the array:", boys);
